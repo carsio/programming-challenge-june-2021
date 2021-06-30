09664721729e67a3cfce8c4949dbaeb8b33a4ff8
Carsio Eddyo <carsio1256@gmail.com>
2021-06-30 14:20:10 -0300

    Add movie service (#5)

---
+++ backend/BusinessApi/Dto/MovieDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Domain.Entity;
+
+namespace BusinessApi.Dto
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<Gender> Genres { get; set; }
+    }
+}
---
+++ backend/BusinessApi/Services/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BusinessApi.Dto;
+
+namespace Business.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<List<MovieDto>> GetMoviesByYearAndGender(int year, int genderId);
+    }
+}
