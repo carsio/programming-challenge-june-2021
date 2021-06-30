e99a1bed9b4f88a03ece66ed35a63be71ec4c450
Carsio <carsio1256@gmail.com>
2021-06-30 14:17:36 -0300

    Add movie service

---
+++ backend/Business/Services/MovieService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Business.Interfaces;
+using BusinessApi.Dto;
+
+namespace Business.Services
+{
+    public class MovieService: IMovieService
+    {
+        public Task<List<MovieDto>> GetMoviesByYearAndGender(int year, int genderId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
