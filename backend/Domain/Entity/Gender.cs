4d9262b47f9c5a39aba1f3db3fe47f26288c2407
Carsio Eddyo <carsio1256@gmail.com>
2021-06-30 00:29:26 -0300

    Base entities (#4)

---
+++ backend/Domain/Entity/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entity
+{
+    public class Gender
+    {
+        [Key, Required] public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Movie> Movies { get; set; }
+    }
+}
