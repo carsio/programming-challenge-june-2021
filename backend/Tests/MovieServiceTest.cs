e985b6b142d7bf409d3685df147ae2be585f29b6
Carsio <carsio1256@gmail.com>
2021-06-30 23:19:58 -0300

    MovieServiceTest

---
+++ backend/Tests/MovieServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Capiflix;
+using Business.Interfaces;
+using Domain.Data;
+using Tests.Config;
+using Xunit;
+
+namespace Tests
+{
+    [Collection(nameof(IntegrationApiTestFixtureCollection))]
+    public class MovieServiceTest : IDisposable
+    {
+        private ApplicationDbContext _applicationDbContext;
+        private readonly IntegrationTestFixture<Startup> _integrationTestFixture;
+
+        public MovieServiceTest(IntegrationTestFixture<Startup> integrationTestFixture)
+        {
+            _integrationTestFixture = integrationTestFixture;
+            _applicationDbContext = _integrationTestFixture.GetApplicationDbContext();
+            Setup();
+        }
+
+        public void Setup()
+        {
+            _integrationTestFixture.InitialDb();
+        }
+        
+        public void Dispose() => _integrationTestFixture.Dispose();
+
+        [Fact]
+        public async Task TestGetMoviesByYearAndGenderShouldReturnData()
+        {
+            var service = _integrationTestFixture.GetService<IMovieService>();
+            
+            var movies1 = await service.GetMoviesByYearAndGender(1995, 2);
+            Assert.Equal(2, movies1.Count);
+            
+            var movies2 = await service.GetMoviesByYearAndGender(1995, 3);
+            Assert.Single(movies2);
+            
+            var movies3 = await service.GetMoviesByYearAndGender(1996, 5);
+            Assert.Equal(2, movies3.Count);
+
+            var movies4 = await service.GetMoviesByYearAndGender(1996, 8);
+            Assert.Single(movies4);
+            
+            var movies5 = await service.GetMoviesByYearAndGender(1996, 9);
+            Assert.Empty(movies5);
+        }
+    }
+}
