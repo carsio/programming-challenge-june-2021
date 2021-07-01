683eec80bf130d467debb2446e431dfcb0d42c2a
Carsio Eddyo <carsio1256@gmail.com>
2021-06-30 23:23:37 -0300

    Feature/add tests (#6)
    * Add test configs
    
    * Movie fake data
    
    * Capiflix.sln
    
    * MovieGender data fake
    
    * MovieServiceTest

---
+++ backend/Tests/Config/AppFactory.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.Config
+{
+    public class AppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseStartup<TStartup>();
+            builder.UseEnvironment("Testing");
+            builder.ConfigureServices(services =>
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemory");
+                    options.ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                });
+            });
+        }
+    }
+}
---
+++ backend/Tests/Config/IntegrationTestFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Api.Capiflix;
+using Domain.Data;
+using Domain.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationApiTestFixtureCollection))]
+    public class IntegrationApiTestFixtureCollection : ICollectionFixture<IntegrationTestFixture<Startup>>
+    {
+    }
+    
+    public class IntegrationTestFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public AppFactory<TStartup> Factory;
+        private ApplicationDbContext _applicationDbContext;
+        private IServiceScope _serviceScope;
+
+        public IntegrationTestFixture()
+        {
+            Factory = new AppFactory<TStartup>();
+        }
+
+        public ApplicationDbContext GetApplicationDbContext()
+        {
+            var scopeFactory = Factory.Services.GetService<IServiceScopeFactory>();
+            _serviceScope = scopeFactory?.CreateScope();
+            _applicationDbContext = _serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+            return _applicationDbContext;
+        }
+
+        public void SetFakeData<TF>() where TF : class
+        {
+            var fakeData = FakeData.GetFakeDataByClass<TF>();
+            _applicationDbContext.Set<TF>().AddRange(fakeData);
+        }
+
+        public T GetService<T>() where T : class => _serviceScope.ServiceProvider.GetService<T>();
+
+        public void ResetDb() => _applicationDbContext.Database.EnsureDeleted();
+
+        public void InitialDb()
+        {
+            SetFakeData<Movie>();
+            SetFakeData<Gender>();
+            SetFakeData<MovieGender>();
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void Dispose() => Factory.Dispose();
+    }
+}
