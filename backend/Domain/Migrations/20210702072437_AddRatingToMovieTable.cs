7dac57070442f62903ea49a86b9e5f869fc28ef7
Carsio Eddyo <carsio1256@gmail.com>
2021-07-02 04:37:17 -0300

    Add rating to movie table (#9)

---
+++ backend/Domain/Migrations/20210702072437_AddRatingToMovieTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domain.Migrations
+{
+    public partial class AddRatingToMovieTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Movies",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movies");
+        }
+    }
+}
